4453bbbe07c2e8913e8b5201ddffa4be2c19a1e0
alexy201 <alexy200412@gmail.com>
2022-06-22 22:26:10 -0700

    added PUT

--- main.go
+++ main.go
@@ -3,24 +3,9 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
-	"net/http"
-	"time"
-
-	"github.com/rs/xid"
 
 	"github.com/gin-gonic/gin"
 )
-
-type Recipe struct {
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
-}
-
-var recipes []Recipe
 
 func init() {
 	recipes = make([]Recipe, 0)
@@ -28,26 +13,10 @@ func init() {
 	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
-func NewRecipeHandler(c *gin.Context) {
-	var recipe Recipe
-	if err := c.ShouldBindJSON(&recipe); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": err.Error()})
-		return
-	}
-	recipe.ID = xid.New().String()
-	recipe.PublishedAt = time.Now()
-	recipes = append(recipes, recipe)
-	c.JSON(http.StatusOK, recipe)
-}
-
-func ListRecipesHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, recipes)
-}
-
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
 }
---
+++ models.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+)
+
+var recipes []Recipe
+
+type Recipe struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
