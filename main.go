f1bb962ac8036897e8c1312d16c613812ce1f1ee
alexy201 <alexy200412@gmail.com>
2022-06-24 02:10:50 -0700

    fixed

--- main.go
+++ main.go
@@ -28,20 +28,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-var ctx context.Context
-var err error
-var client *mongo.Client
-
 var recipesHandler *handlers.RecipesHandler
 
 func init() {
-	ctx = context.Background()
-	client, err = mongo.Connect(ctx,
-		options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	//docker service create -d --name mongodb -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD_FILE=/run/secrets/mongodb_password -p 27017:27017 mongo:latest
+	//MONGO_URI="mongodb://localhost:27017/test?authSource=admin" MONGO_DATABASE=demo go run main.go
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
 	if err = client.Ping(context.TODO(),
 		readpref.Primary()); err != nil {
 		log.Fatal(err)
 	}
+	collection := client.Database(os.Getenv("MONGO_DATABASE")).Collection("recipes")
+	recipesHandler = handlers.NewRecipesHandler(ctx, collection)
 	log.Println("Connected to MongoDB")
 }
 
